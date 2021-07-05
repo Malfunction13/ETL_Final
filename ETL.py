824756566306b2931c30addc8c87d127e00a5a2e
Malfunction13 <53430563+Malfunction13@users.noreply.github.com>
2021-07-05 13:13:58 +0100

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -2,3 +2,4 @@ from Controllers.ETLController import ETLController
 
 ETL = ETLController
 ETL().source("json").sink("postgres").run()
+#source args can be "json" or "simulation" and sink args can be "postgres" or "stdout"
